f03d08ed603d19ba305d2b9036f3a1ff3c32e3ee
Timo Srinarmwong <125150205+tsrinarmwong@users.noreply.github.com>
2025-03-11 17:31:28 -0400

    Added GitHub Action

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require("@actions/core");
+const { signLicense } = require("./src/signLicense");
+
+try {
+    const licenseKey = core.getInput("license_key");
+    const privateKey = core.getInput("private_key");
+    const signature = signLicense(licenseKey, privateKey);
+
+    core.setOutput("signature", signature);
+} catch (error) {
+    core.setFailed(error.message);
+}
